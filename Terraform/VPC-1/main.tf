e3d7d92d0a2961a629720993e0007b852dc87fa1
omeshwar <73010142+omeshwarkandari@users.noreply.github.com>
2022-10-17 19:15:27 +0530

    Update main.tf

--- Terraform/VPC-1/main.tf
+++ Terraform/VPC-1/main.tf
@@ -8,26 +8,26 @@ resource "aws_vpc" "demo" {
 }
 
 
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public-subnet" {
   count                   = length(var.public_subnet_cidr_blocks)
   vpc_id                  = aws_vpc.demo.id
   cidr_block              = var.public_subnet_cidr_blocks[count.index]
   availability_zone       = var.availability_zones[count.index]
   map_public_ip_on_launch = true
   tags = {
-    Name = "${var.namespace}-public-${count.index+1}"
+    Name = "${var.namespace}-public-subnet-${count.index+1}"
   }
 }
 
 
-resource "aws_subnet" "private" {
+resource "aws_subnet" "private-subnet" {
   count = length(var.private_subnet_cidr_blocks)
 
   vpc_id            = aws_vpc.demo.id
   cidr_block        = var.private_subnet_cidr_blocks[count.index]
   availability_zone = var.availability_zones[count.index]
   tags = {
-    Name = "${var.namespace}-private-${count.index+1}"
+    Name = "${var.namespace}-private-subnet-${count.index+1}"
   }
 }
 
@@ -42,7 +42,7 @@ resource "aws_internet_gateway" "itg" {
 
 ### This will create both local route as well as route for 0.0.0.0/0
 
-resource "aws_route_table" "public_rt" {
+resource "aws_route_table" "public-route" {
   vpc_id = aws_vpc.demo.id
   route {
 	cidr_block = "0.0.0.0/0"
@@ -50,19 +50,19 @@ resource "aws_route_table" "public_rt" {
   }
   depends_on = [aws_internet_gateway.itg]
   tags = {
-    Name = "${var.namespace}-public_rt"
+    Name = "${var.namespace}-public-route"
   }
 }	
 
-resource "aws_route_table_association" "public" {
+resource "aws_route_table_association" "public-route" {
   count = length(var.public_subnet_cidr_blocks)
 
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public_rt.id
+  subnet_id      = aws_subnet.public-subnet[count.index].id
+  route_table_id = aws_route_table.public-route.id
 }
 
 
-resource "aws_route_table" "private_rt" {
+resource "aws_route_table" "private-route" {
     vpc_id = aws_vpc.demo.id
 	count = length(var.private_subnet_cidr_blocks)
 	route {
@@ -70,14 +70,14 @@ resource "aws_route_table" "private_rt" {
 		nat_gateway_id = aws_nat_gateway.ntg[count.index].id	
 	}
     tags = {
-    Name = "${var.namespace}-private_rt-${count.index+1}"
+    Name = "${var.namespace}-private-route-${count.index+1}"
   }
 }
 
-resource "aws_route_table_association" "private_rt" {
+resource "aws_route_table_association" "private-route" {
   count          = length(var.private_subnet_cidr_blocks)
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private_rt[count.index].id
+  subnet_id      = aws_subnet.private-subnet[count.index].id
+  route_table_id = aws_route_table.private-route[count.index].id
 }
 
 
@@ -87,7 +87,7 @@ resource "aws_eip" "nat" {
   count = length(var.private_subnet_cidr_blocks)
   vpc = true
   tags = {
-    Name = "${var.namespace}-eip${count.index+1}"
+    Name = "${var.namespace}-eip-${count.index+1}"
   }
 }
 
@@ -95,7 +95,7 @@ resource "aws_nat_gateway" "ntg" {
   depends_on = [aws_internet_gateway.itg]
   count = length(var.private_subnet_cidr_blocks)  
   allocation_id = aws_eip.nat[count.index].id
-  subnet_id     = aws_subnet.public[count.index].id
+  subnet_id     = aws_subnet.public-subnet[count.index].id
   tags = {
     Name = "${var.namespace}-ntg-${count.index+1}"
   }
